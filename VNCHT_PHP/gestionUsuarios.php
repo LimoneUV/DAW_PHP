fb91dfd4406b21718e29a3b14942d38c720cfea1
LimoneUV <limone@alumni.uv.es>
2017-06-03 17:48:31 +0200

    gestionUsuarios

--- VNCHT_PHP/gestionUsuarios.php
+++ VNCHT_PHP/gestionUsuarios.php
@@ -1,4 +1,4 @@
-<?php
+<?php?>
 
 /* 
  * To change this license header, choose License Headers in Project Properties.
@@ -6,3 +6,106 @@
  * and open the template in the editor.
  */
 
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form form method="post" action="gestionUsuarios.php">
+            <table>
+                <tr>
+                    <td><label>User: </label></td>
+                    <td><input type="text" name="nick"/></td>
+                </tr>
+                <tr>
+                    <td><input type="submit" value="Buscar" name="buscar"/></td>
+                </tr>
+            </table>
+        </form>
+        <?php
+            $recurso=mysqli_connect("localhost","root","","tiendaonline");
+            if(mysqli_connect_error()){
+                printf("Error conectando a la BD tiendaonline: %s",mysqli_connect_error());
+                exit();
+}
+            if(isset($_POST['buscar'])){
+            $nick=$_POST['nick'];
+            ($resultado=mysqli_query($recurso,
+                                    "SELECT password,nombre,apellidos,email,direccion,fecha_nac,tipo,estado"
+                    . "              FROM user WHERE nick LIKE '$nick'"));
+            
+            while($fila=mysqli_fecth_row($resultado)){
+                $password=$fila[0];
+                $nombre=$fila[1];
+                $apellidos=$fila[2];
+                $email=$fila[3];
+                $direccion=$fila[4];
+                $fecha_nac=$fila[5];
+                $tipo=$fila[6];
+                $estado=$fila[7];
+            }
+            }
+        ?>
+        <form method="post" action="gestionUsuarios.php">
+            <table>
+                <tr>
+                    <td><label>User:</label></td>
+                    <td><input type="text" name="nickf" value="<?PHP echo $nick ?>" readonly="readonly"/></td>
+                    <td><label>Password:</label></td>
+                    <td><input type="text" name="password" value="<?PHP echo $password ?>"/></td>
+                </tr>
+                <tr>
+                    <td><label>Nombre:</label></td>
+                    <td><input type="text" name="nombre" value="<?PHP echo $nombre ?>"/></td>
+                    <td><label>Apellidos:</label></td>
+                    <td><input type="text" name="apellidos" value="<?PHP echo $apellidos ?>"/></td>
+                </tr>
+                <tr>
+                    <td><label>Email:</label></td>
+                    <td><input type="text" name="email" value="<?PHP echo $email ?>"/></td>
+                </tr>
+                <tr>
+                    <td><label>Dirección:</label></td>
+                    <td><input type="text" name="direccion" value="<?PHP echo $direccion ?>"/></td>
+                </tr>
+                <tr>
+                    <td><label>Fecha nacimiento:</label></td>
+                    <td><input type="text" name="fecha_nac" value="<?PHP echo $fecha_nac ?>"/></td>
+                </tr>
+                <tr>
+                    <td><label>Tipo:</label></td>
+                    <td><input type="text" name="tipo" value="<?PHP echo $tipo ?>"/></td>
+                    <td><label>Estado:</label></td>
+                    <td><input type="text" name="estado" value="<?PHP echo $estado ?>"/></td>
+                </tr>
+                <tr>
+                    <td><input type="submit" name="modificar" value="Modificar"/></td>
+                    <td><input type="submit" name="eliminar" value="Eliminar"/></td>
+                </tr>
+            </table>
+        </form>
+        <?PHP
+            $nickf=$_POST['nickf'];
+            $passwordf=$_POST['password'];
+            $nombref=$_POST['nombre'];
+            $apellidosf=$_POST['apellidos'];
+            $emailf=$_POST['email'];
+            $direccionf=$_POST['direccion'];
+            $fecha_nacf=$_POST['fecha_nac'];
+            $tipof=$_POST['tipo'];
+            $estadof=$_POST['estado'];
+            if(isset($_POST['modificar'])){
+                ($resultado=mysqli_query($recurso,
+                                    "UPDATE user"
+                        . "         SET nombre='$nombref',apellidos='$apellidosf',password='$passwordf',email='$emailf',direccion='$direccionf',fecha_nac='$fecha_nacf',tipo='$tipof',estado='$estadof'"
+                        . "         WHERE nick LIKE '$nickf'"));
+            }
+            if(isset($_POST['eliminar'])){
+                ($resultado=mysqli_query($recurso,
+                                    "UPDATE FROM user"
+                        . "         WHERE nick LIKE '$nickf'"));
+            }
+        ?>
+    </body>
+</html>
